0b1f28b11f5d6af7f8151bed8b91234b808dddbc
ivan <ivan@DESKTOP-NELLSAH>
2025-03-10 18:04:15 +0300

    Feat: add providers for RTK and config base store

--- ai-personal-account/src/App.tsx
+++ ai-personal-account/src/App.tsx
@@ -1,32 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Button, Typography, Container } from '@mui/material';
-
+import { Button, Container, Typography } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import { store } from './store/store';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Container>
-                <Typography variant="h3" component="h1" gutterBottom>
-                  Hello, MUI!
-                </Typography>
-                <Button variant="contained" color="primary">
-                  Click Me
-                </Button>
-              </Container>
-            </>
-          }
-        />
-        <Route path="/about" element={<>kek2</>} />
-        <Route path="/contact" element={<>kek3</>} />
-      </Routes>
-    </Router>
+    <>
+      <SnackbarProvider
+        maxSnack={3}
+        preventDuplicate
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      />
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Container>
+                    <Typography variant="h3" component="h1" gutterBottom>
+                      Hello, MUI!
+                    </Typography>
+                    <Button variant="contained" color="primary">
+                      Click Me
+                    </Button>
+                  </Container>
+                </>
+              }
+            />
+            <Route path="/about" element={<>kek2</>} />
+            <Route path="/contact" element={<>kek3</>} />
+            <Route path="/login" element={<Login></Login>} />
+          </Routes>
+        </Router>
+      </Provider>
+    </>
   );
 }
 
