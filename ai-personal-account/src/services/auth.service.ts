9e85886374894636192772883a80b88906f6c0a8
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-03-19 12:16:55 +0300

    feat: auth-service

---
+++ ai-personal-account/src/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { ILoginRequest, ILoginResponse } from '../interfaces/auth/auth.interface.ts';
+import baseQueryHandler from './base-query.ts';
+
+export const authService = createApi({
+  reducerPath: 'authService',
+  tagTypes: ['auth'],
+  baseQuery: baseQueryHandler,
+  endpoints: builder => ({
+    login: builder.mutation<ILoginResponse, ILoginRequest>({
+      query: data => ({
+        url: 'login',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data,
+        formData: true
+      })
+    }),
+    logout: builder.mutation<void, void>({
+      query: () => ({
+        url: 'logout',
+        method: 'POST',
+        providesTags: ['auth'],
+      }),
+      invalidatesTags: ['auth']
+    }),
+
+  })
+});
+
+export const {
+  useLoginMutation,
+  useLogoutMutation
+} = authService;
