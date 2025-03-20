40e36eee681557e823fe35e52e9fd7049e1f44ab
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-03-20 22:29:10 +0300

    feat: add form

--- ai-personal-account/src/pages/Company/Company.tsx
+++ ai-personal-account/src/pages/Company/Company.tsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import ClientDataForm from '../../components/forms/ClientDataForm';
+import { Box } from '@mui/material';
+
 interface iCompany {}
 
 export default function Company({}: iCompany) {
-  return <>Company</>;
+  return (
+    <Box
+      sx={{
+        p: { xs: 1, sm: 2, md: 4 },
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        overflow: 'scroll',
+      }}
+    >
+      <ClientDataForm />
+    </Box>
+  );
 }
