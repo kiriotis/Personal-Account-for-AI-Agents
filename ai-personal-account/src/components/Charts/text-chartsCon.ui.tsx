fb8c4917dbf4644d8b6b65eb04d2b4ac6072e977
ivan <ivan@DESKTOP-NELLSAH>
2025-03-24 16:12:06 +0300

    Feat: add second graph for text boot

---
+++ ai-personal-account/src/components/Charts/text-chartsCon.ui.tsx
@@ -0,0 +1,84 @@
+import { Box, Typography } from '@mui/material';
+import { axisClasses, BarChart } from '@mui/x-charts';
+import { useState } from 'react';
+
+interface ChartDataItem {
+  direction: string;
+  conversion: number;
+}
+
+type SeriesItem = {
+  dataKey: keyof ChartDataItem;
+  label: string;
+  valueFormatter: (value: number | null) => string;
+};
+
+export function valueFormatter(value: number | null): string {
+  return `${value}%`;
+}
+
+const dataset = [
+  { direction: 'Москва', conversion: 20.0 },
+  { direction: 'Стамбул', conversion: 35.5 },
+  { direction: 'Минск', conversion: 26.89 },
+  { direction: 'Санкт-Петербург', conversion: 18.3 },
+  { direction: 'Сочи', conversion: 42.1 },
+  { direction: 'Казань', conversion: 15.7 },
+  { direction: 'Новосибирск', conversion: 32.4 },
+  { direction: 'Сибирь', conversion: 22.7 },
+  { direction: 'Гродно', conversion: 22.7 },
+  { direction: 'Кеокльдесина', conversion: 22.7 },
+  { direction: 'Городина', conversion: 152.7 },
+];
+
+// Сортируем данные по убыванию conversion
+const sortedDataset = [...dataset].sort((a, b) => b.conversion - a.conversion);
+
+const chartSetting = {
+  yAxis: [
+    {
+      label: 'Conversion (%)',
+    },
+  ],
+  height: 250,
+  sx: {
+    [`.${axisClasses.left} .${axisClasses.label}`]: {
+      transform: 'translate(-10px, 0)',
+    },
+  },
+};
+
+export default function ConversionChart() {
+  const [series] = useState<SeriesItem[]>([
+    {
+      dataKey: 'conversion',
+      label: 'Conversion Rate',
+      valueFormatter: valueFormatter,
+    },
+  ]);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        width: '100%',
+        p: 0,
+      }}
+    >
+      <Typography variant="h5">{'Коверсия по городам'}</Typography>
+      <BarChart
+        dataset={sortedDataset} // Используем отсортированный массив
+        xAxis={[
+          {
+            scaleType: 'band',
+            dataKey: 'direction',
+          },
+        ]}
+        series={series}
+        {...chartSetting}
+      />
+    </Box>
+  );
+}
