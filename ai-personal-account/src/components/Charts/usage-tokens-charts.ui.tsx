b4139d51cc7e89754ec6ca8d7cc11d5fc9b0fe97
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-03-24 16:26:23 +0300

    fix: edit charts

---
+++ ai-personal-account/src/components/Charts/usage-tokens-charts.ui.tsx
@@ -0,0 +1,114 @@
+import {
+  Box,
+  Typography,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  IconButton,
+} from '@mui/material';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { useState, useEffect } from 'react';
+import { generateMockGraphData } from '../../utils/helpers/mockGraphData';
+
+export default function UsageTokensCharts() {
+  const [period, setPeriod] = useState<'7' | '30' | '90'>('7');
+  const [chartData, setChartData] = useState(generateMockGraphData(7));
+
+  useEffect(() => {
+    const data = generateMockGraphData(parseInt(period));
+    setChartData(data);
+  }, [period]);
+  const handlePeriodChange = (event: SelectChangeEvent) => {
+    setPeriod(event.target.value as '7' | '30' | '90');
+  };
+
+  return (
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h5">
+          {'Статистика по кол-ву использованных токенов'}
+        </Typography>
+      </Box>
+      <Box
+        sx={{ display: 'flex', width: '100%', justifyContent: 'space-between', flexWrap:'wrap' }}
+      >
+        <FormControl variant="standard">
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={period}
+            label="Age"
+            onChange={handlePeriodChange}
+          >
+            <MenuItem value={'7'}>7 days</MenuItem>
+            <MenuItem value={'30'}>30 days</MenuItem>
+            <MenuItem value={'90'}>90 days</MenuItem>
+          </Select>
+        </FormControl>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 2,
+          }}
+        >
+          <IconButton disabled>
+            <ChevronLeftIcon />
+          </IconButton>
+          <Typography variant="body1" sx={{ margin: '0 10px' }}>
+            {`Период: 01.02.2025 - 07.02.2027`}
+          </Typography>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexGrow: 1,
+          flexDirection: 'column',
+          width: '100%',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+          }}
+        >
+          <BarChart
+            xAxis={[{ scaleType: 'band', data: chartData.labels }]}
+            series={[
+              { data: chartData.input, label: 'Вход', stack: 'assets' },
+              { data: chartData.output, label: 'Выход', stack: 'assets' },
+            ]}
+            borderRadius={6}
+            sx={{
+              // pointerEvents: { xs: 'none', sm: 'auto' },
+              transition: { xs: 'none', sm: 'all 0.3s ease' },
+              width: '100%',
+            }}
+            height={300}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- ai-personal-account/src/pages/Usage/Usage.tsx
+++ ai-personal-account/src/pages/Usage/Usage.tsx
@@ -1,29 +1,10 @@
 import { Box } from '@mui/material';
-import UsageCharts from '../../components/Charts/usage-charts.ui';
+import UsageTokensCharts from '../../components/Charts/usage-tokens-charts.ui';
+import UsageMessagesCharts from '../../components/Charts/usage-messages-charts.ui';
 
 interface iUsage {}
 
 export default function Usage({}: iUsage) {
-  const generateMockData = (days: number) => {
-    const labels = Array.from({ length: days }, (_, i) => `${i + 1}.02`);
-    const input = Array(days)
-      .fill(0)
-      .map(() => Math.floor(Math.random() * 5000));
-    const output = Array(days)
-      .fill(0)
-      .map(() => Math.floor(Math.random() * 5000));
-    return { labels, input, output };
-  };
-
-  const data: Record<
-    number,
-    { labels: string[]; input: number[]; output: number[] }
-  > = {
-    7: generateMockData(7),
-    30: generateMockData(30),
-    90: generateMockData(90),
-  };
-
   return (
     <Box
       sx={{
@@ -32,24 +13,11 @@ export default function Usage({}: iUsage) {
         flexDirection: 'column',
         flexGrow: 1,
         overflow: 'scroll',
+        gap: 2,
       }}
     >
-      <UsageCharts
-        data={data}
-        title={'Статистика по кол-ву использованных токенов'}
-        series={[
-          { label: 'Вход' },
-          { label: 'Выход' },
-        ]}
-      />
-      <UsageCharts
-        data={data}
-        title={'Статистика по кол-ву сообщений'}
-        series={[
-          { label: 'Получено' },
-          { label: 'Отправлено' },
-        ]}
-      />
+      <UsageTokensCharts />
+      <UsageMessagesCharts />
     </Box>
   );
 }
