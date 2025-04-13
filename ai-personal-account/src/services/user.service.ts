2fbd981a7e4238feac3bd4cdd9c707fff45ca7c6
ivan <ivan@DESKTOP-NELLSAH>
2025-04-13 21:45:41 +0300

    Feat: add query for  company form

--- ai-personal-account/src/services/user.service.ts
+++ ai-personal-account/src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import baseQueryHandler from './base-query.ts';
 import { iUser } from '../interfaces/user/user.interface.ts';
+import { patch } from '@mui/material';
 
 export const authUser = createApi({
   reducerPath: 'authUser',
@@ -18,7 +19,20 @@ export const authUser = createApi({
         formData: true,
       }),
     }),
+    patchUser: builder.mutation<
+      iUser,
+      { id: string; userData: Partial<Omit<iUser, 'id'>> }
+    >({
+      query: ({ id, userData }) => ({
+        url: `/users/${id}`,
+        method: 'PATCH',
+        body: userData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
   }),
 });
 
-export const {useUsermeMutation} = authUser;
+export const { useUsermeMutation, usePatchUserMutation } = authUser;
